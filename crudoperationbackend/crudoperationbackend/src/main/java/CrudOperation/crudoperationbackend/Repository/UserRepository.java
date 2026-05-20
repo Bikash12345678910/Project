650d43f7d8dcf8763a8054fe0f563775b87acb1f
Bikash Sau <bikashkumarsau0012@gmail.com>
2026-05-20 12:05:39 +0530

    Delete crudoperationbackend directory

--- crudoperationbackend/crudoperationbackend/src/main/java/CrudOperation/crudoperationbackend/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package CrudOperation.crudoperationbackend.Repository;
-
-import CrudOperation.crudoperationbackend.Entity.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<Users,Integer> {
-}
